8f15760b5dfa7e9a96b5f27da23709105d01c15f
volcha <57828516+volcha@users.noreply.github.com>
2023-06-07 17:38:33 +0300

    Повышение резкости

--- blur.m
+++ blur.m
@@ -1,22 +1,22 @@
-close all; clear all; 
+close all; clear all;
 
-orig_image = imread('88.jpg'); % ñ÷èòûâàíèå èñõîäíîãî èçîáðàæåíèÿ
-orig_image = rgb2gray(orig_image); % ïåðåâîä èçîáðàæåíèÿ â ïîëóòîí. âèä
+orig_image = imread('88.jpg'); % считывание исходного изображения
+orig_image = rgb2gray(orig_image); % перевод изображения в полутон. вид
 orig_image = im2double(orig_image);
-figure(1); imshow(orig_image,[]); % âûâîä èñõîäíîãî èçîáðàæåíèÿ
+figure(1); imshow(orig_image,[]); % вывод исходного изображения
 
-[n_size,m_size] = size(orig_image); % ðàçìåðû èçîáðàæåíèÿ
-number_of_pixels = n_size * m_size; % êîëè÷åñòâî ïèêñåëåé
+[n_size,m_size] = size(orig_image); % размеры изображения
+number_of_pixels = n_size * m_size; % количество пикселей
 
-% óñèëåíèå ðàçìûòîñòè
+% усиление размытости
 filtr = fspecial('gaussian',[7 7],4);
 blur_image = imfilter(orig_image,filtr,'same');
 figure(2); imshow(blur_image);
 %blur_image = orig_image;
 
-wnm = 'db10'; % âûáîð òèïà âåéâëåòà
-N = 5; %óðîâåíü ðàçëîæåíèÿ
-[c,s] = wavedec2(blur_image,N,wnm); % äåêîìïîçèöèÿ
+wnm = 'db10'; % выбор типа вейвлета
+N = 5; %уровень разложения
+[c,s] = wavedec2(blur_image,N,wnm); % декомпозиция
 s
 [mc,nc]=size(c);
 nzs = 0;
@@ -29,11 +29,11 @@ for i = 1:1:N
         nzs = nzs + s(i,1)*s(i,2)*3;
     end
     Cn(1:nzs) = zeros(1,nzs);
-    X = waverec2(Cn,s,wnm); % ðåêîíñòðóêöèÿ
+    X = waverec2(Cn,s,wnm); % реконструкция
     X = imadjust(X);
     no_blur = no_blur + blur_image + X;
 end
-no_blur = no_blur -  min(no_blur(:)); %íîðìèðîâêà
+no_blur = no_blur -  min(no_blur(:)); %нормировка
 no_blur = no_blur/max(no_blur(:));
 no_blur = imadjust(no_blur); 
 figure(44); imshow(no_blur,[]);
@@ -41,9 +41,9 @@ subtraction = orig_image - no_blur;
 pow = subtraction.^2;
 mse = sum(pow(:)) / number_of_pixels
 
-W = [ 1 1 1; 1 -8 1; 1 1 1];% ôèëüòð Ëàïëàñèàíà
-G = imfilter(blur_image,W,'replicate'); % ôèëüòðàöèÿ
-GL = blur_image-G; % óâåëè÷åíèå ðåçêîñòè
+W = [ 1 1 1; 1 -8 1; 1 1 1]; % фильтр Лапласиана
+G = imfilter(blur_image,W,'replicate');
+GL = blur_image-G; % увеличение резкости
 GL = imadjust(GL); 
 figure(5); imshow(GL,[]);
 subtraction = orig_image - GL;
